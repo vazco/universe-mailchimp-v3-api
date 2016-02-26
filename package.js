3af8ae8544154572bd643428de20bc9f0ec24c6b
BobcatMK <matthew.kilan@gmail.com>
2016-02-26 16:39:44 +0100

    fix for deps

--- package.js
+++ package.js
@@ -4,10 +4,12 @@ Package.describe({
     summary: 'Wrapper for mailchimp APIv3'
 });
 
+Npm.depends({'lodash': '4.4.0'});
+
 Package.onUse(function (api) {
     api.versionsFrom('METEOR@1.3-modules-beta.4');
 
-    api.use('ecmascript');
+    api.use(['ecmascript']);
 
     api.mainModule('index.js');
 });
